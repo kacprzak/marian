4531e478250a83926121e7307d34f4d37a8aa6b6
Marcin Kacprzak <martin.kacprzak@gmail.com>
2012-12-29 21:49:05 +0100

    Minor changes

--- Engine.cpp
+++ Engine.cpp
@@ -206,8 +206,10 @@ void Engine::initializeSDL()
   SDL_GL_SetAttribute(SDL_GL_RED_SIZE,   8);
   SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
   SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,  8);
-  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+  //SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, 1);
 
   char env[] = "SDL_VIDEO_CENTERED=center";
   SDL_putenv(env);
