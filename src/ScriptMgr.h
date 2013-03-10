6fe1d59488aded80c05b463d078b027ba82ccda5
Marcin Kacprzak <martin.kacprzak@gmail.com>
2013-03-10 20:04:54 +0100

    Changed lua include to lua.hpp

--- src/ScriptMgr.h
+++ src/ScriptMgr.h
@@ -4,12 +4,7 @@
 
 #include <exception>
 #include <boost/utility.hpp>
-
-extern "C" {
-  #include <lua.h>
-  #include <lualib.h>
-  #include <lauxlib.h>
-}
+#include <lua.hpp>
 
 //------------------------------------------------------------------------------
 
