daa2d1c274c3ecd432536195df1593a01e9b3d49
Marcin Kacprzak <martin.kacprzak@gmail.com>
2013-03-18 20:53:46 +0100

    Console gui finished

--- src/GuiMgr.cpp
+++ src/GuiMgr.cpp
@@ -5,7 +5,12 @@
 #include <RendererModules/OpenGL/CEGUIOpenGLRenderer.h>
 #include <iostream>
 
+static CEGUI::uint SDLKeyToCEGUIKey(SDLKey key);
+
+
 GuiMgr::GuiMgr()
+    : m_consoleWindow(nullptr)
+    , m_consoleVisible(true)
 {
     CEGUI::OpenGLRenderer::bootstrapSystem();
 
@@ -33,19 +38,20 @@ GuiMgr::GuiMgr()
     CEGUI::SchemeManager::getSingleton().create("TaharezLook.scheme");
     CEGUI::SchemeManager::getSingleton().create("VanillaSkin.scheme");
 
-    CEGUI::Window *consoleWindow
-        = CEGUI::WindowManager::getSingleton().loadWindowLayout("VanillaConsole.layout");
+    m_consoleWindow = CEGUI::WindowManager::getSingleton().loadWindowLayout("VanillaConsole.layout");
 
     // Set the defaults
     CEGUI::System::getSingleton().setDefaultFont("DejaVuSans-10");
-    CEGUI::System::getSingleton().setDefaultMouseCursor("Vanilla-Images", "MouseArrow");
+    //CEGUI::System::getSingleton().setDefaultMouseCursor("Vanilla-Images", "MouseArrow");
 
     CEGUI::Window* myRoot =
         CEGUI::WindowManager::getSingleton().createWindow("DefaultWindow", "_MasterRoot");
 
     CEGUI::System::getSingleton().setGUISheet(myRoot);
+    CEGUI::System::getSingleton().getGUISheet()->addChildWindow(m_consoleWindow);
 
-    CEGUI::System::getSingleton().getGUISheet()->addChildWindow(consoleWindow);
+    registerHandlers();
+    setConsoleVisible(false);
 
     std::cout << "GuiMgr created\n";
 }
@@ -81,23 +87,29 @@ bool GuiMgr::processInput(const SDL_Event& e)
  
         /* key down */
     case SDL_KEYDOWN:
-        /* to tell CEGUI that a key was pressed, we inject the scancode. */
-        CEGUI::System::getSingleton().injectKeyDown(e.key.keysym.scancode);
-        /* as for the character it's a litte more complicated.
-         * we'll use for translated unicode value.
-         * this is described in more detail below.
-         */
-        if ((e.key.keysym.unicode & 0xFF80) == 0) {
-            CEGUI::System::getSingleton().injectChar(e.key.keysym.unicode & 0x7F);
+        {
+            CEGUI::uint kc = SDLKeyToCEGUIKey(e.key.keysym.sym);
+            if (kc == CEGUI::Key::F12) {
+                setConsoleVisible(!m_consoleVisible);
+            }
+
+            CEGUI::System::getSingleton().injectKeyDown(kc);
+            /* as for the character it's a litte more complicated.
+             * we'll use for translated unicode value.
+             * this is described in more detail below.
+             */
+            if ((e.key.keysym.unicode & 0xFF80) == 0) {
+                CEGUI::System::getSingleton().injectChar(e.key.keysym.unicode & 0x7F);
+            }
+            break;
         }
-        break;
- 
         /* key up */
     case SDL_KEYUP:
-        /* like before we inject the scancode directly. */
-        CEGUI::System::getSingleton().injectKeyUp(e.key.keysym.scancode);
-        break;
- 
+        {
+            CEGUI::uint kc = SDLKeyToCEGUIKey(e.key.keysym.sym);
+            CEGUI::System::getSingleton().injectKeyUp(kc);
+            break;
+        }
     case SDL_VIDEORESIZE:
         CEGUI::System::getSingleton().notifyDisplaySizeChanged(CEGUI::Size(e.resize.w,
                                                                            e.resize.h));
@@ -160,4 +172,200 @@ void GuiMgr::handle_mouse_up(Uint8 button)
         CEGUI::System::getSingleton().injectMouseButtonUp(CEGUI::RightButton);
         break;
     }
+}
+
+//------------------------------------------------------------------------------
+
+void GuiMgr::setConsoleVisible(bool visible)
+{
+    m_consoleWindow->setVisible(visible);
+    m_consoleVisible = visible;
+ 
+    CEGUI::Window *editBox = m_consoleWindow->getChild("Vanilla/Console/Editbox");
+    if(visible)
+        editBox->activate();
+    else
+        editBox->deactivate();
+}
+
+//------------------------------------------------------------------------------
+
+bool GuiMgr::isConsoleVisible()
+{
+    return m_consoleWindow->isVisible();
+}
+
+//------------------------------------------------------------------------------
+
+void GuiMgr::registerHandlers()
+{
+    m_consoleWindow->getChild("Vanilla/Console/Submit")->
+        subscribeEvent(CEGUI::PushButton::EventClicked,
+                       CEGUI::Event::Subscriber(&GuiMgr::handle_SendButtonPressed, this));
+
+    m_consoleWindow->getChild("Vanilla/Console/Editbox")->
+        subscribeEvent(CEGUI::Editbox::EventTextAccepted,
+                       CEGUI::Event::Subscriber(&GuiMgr::handle_TextSubmitted, this));
+}
+
+//------------------------------------------------------------------------------
+
+bool GuiMgr::handle_TextSubmitted(const CEGUI::EventArgs& e)
+{
+    const CEGUI::WindowEventArgs* args = static_cast<const CEGUI::WindowEventArgs*>(&e);
+ 
+    CEGUI::String msg = m_consoleWindow->getChild("Vanilla/Console/Editbox")->getText();
+ 
+    parseText(msg);
+ 
+    m_consoleWindow->getChild("Vanilla/Console/Editbox")->setText("");
+ 
+    return true;
+}
+
+//------------------------------------------------------------------------------
+
+bool GuiMgr::handle_SendButtonPressed(const CEGUI::EventArgs& /*e*/)
+{
+    CEGUI::String msg = m_consoleWindow->getChild("Vanilla/Console/Editbox")->getText();
+    parseText(msg);
+    m_consoleWindow->getChild("Vanilla/Console/Editbox")->setText("");
+ 
+    return true;
+}
+
+//------------------------------------------------------------------------------
+
+void GuiMgr::parseText(CEGUI::String inMsg)
+{
+    outputText(inMsg);
+}
+
+//------------------------------------------------------------------------------
+
+void GuiMgr::outputText(CEGUI::String inMsg, CEGUI::colour /*colour*/)
+{
+    CEGUI::MultiLineEditbox *outputWindow =
+        static_cast<CEGUI::MultiLineEditbox *>(m_consoleWindow->getChild("Vanilla/Console/History"));
+        
+    outputWindow->setText(inMsg);
+}
+
+/************************************************************************
+    Translate a SDLKey to the proper CEGUI::Key
+*************************************************************************/
+CEGUI::uint SDLKeyToCEGUIKey(SDLKey key)
+{
+    using namespace CEGUI;
+    switch (key) {
+    case SDLK_BACKSPACE:    return Key::Backspace;
+    case SDLK_TAB:          return Key::Tab;
+    case SDLK_RETURN:       return Key::Return;
+    case SDLK_PAUSE:        return Key::Pause;
+    case SDLK_ESCAPE:       return Key::Escape;
+    case SDLK_SPACE:        return Key::Space;
+    case SDLK_COMMA:        return Key::Comma;
+    case SDLK_MINUS:        return Key::Minus;
+    case SDLK_PERIOD:       return Key::Period;
+    case SDLK_SLASH:        return Key::Slash;
+    case SDLK_0:            return Key::Zero;
+    case SDLK_1:            return Key::One;
+    case SDLK_2:            return Key::Two;
+    case SDLK_3:            return Key::Three;
+    case SDLK_4:            return Key::Four;
+    case SDLK_5:            return Key::Five;
+    case SDLK_6:            return Key::Six;
+    case SDLK_7:            return Key::Seven;
+    case SDLK_8:            return Key::Eight;
+    case SDLK_9:            return Key::Nine;
+    case SDLK_COLON:        return Key::Colon;
+    case SDLK_SEMICOLON:    return Key::Semicolon;
+    case SDLK_EQUALS:       return Key::Equals;
+    case SDLK_LEFTBRACKET:  return Key::LeftBracket;
+    case SDLK_BACKSLASH:    return Key::Backslash;
+    case SDLK_RIGHTBRACKET: return Key::RightBracket;
+    case SDLK_a:            return Key::A;
+    case SDLK_b:            return Key::B;
+    case SDLK_c:            return Key::C;
+    case SDLK_d:            return Key::D;
+    case SDLK_e:            return Key::E;
+    case SDLK_f:            return Key::F;
+    case SDLK_g:            return Key::G;
+    case SDLK_h:            return Key::H;
+    case SDLK_i:            return Key::I;
+    case SDLK_j:            return Key::J;
+    case SDLK_k:            return Key::K;
+    case SDLK_l:            return Key::L;
+    case SDLK_m:            return Key::M;
+    case SDLK_n:            return Key::N;
+    case SDLK_o:            return Key::O;
+    case SDLK_p:            return Key::P;
+    case SDLK_q:            return Key::Q;
+    case SDLK_r:            return Key::R;
+    case SDLK_s:            return Key::S;
+    case SDLK_t:            return Key::T;
+    case SDLK_u:            return Key::U;
+    case SDLK_v:            return Key::V;
+    case SDLK_w:            return Key::W;
+    case SDLK_x:            return Key::X;
+    case SDLK_y:            return Key::Y;
+    case SDLK_z:            return Key::Z;
+    case SDLK_DELETE:       return Key::Delete;
+    case SDLK_KP0:          return Key::Numpad0;
+    case SDLK_KP1:          return Key::Numpad1;
+    case SDLK_KP2:          return Key::Numpad2;
+    case SDLK_KP3:          return Key::Numpad3;
+    case SDLK_KP4:          return Key::Numpad4;
+    case SDLK_KP5:          return Key::Numpad5;
+    case SDLK_KP6:          return Key::Numpad6;
+    case SDLK_KP7:          return Key::Numpad7;
+    case SDLK_KP8:          return Key::Numpad8;
+    case SDLK_KP9:          return Key::Numpad9;
+    case SDLK_KP_PERIOD:    return Key::Decimal;
+    case SDLK_KP_DIVIDE:    return Key::Divide;
+    case SDLK_KP_MULTIPLY:  return Key::Multiply;
+    case SDLK_KP_MINUS:     return Key::Subtract;
+    case SDLK_KP_PLUS:      return Key::Add;
+    case SDLK_KP_ENTER:     return Key::NumpadEnter;
+    case SDLK_KP_EQUALS:    return Key::NumpadEquals;
+    case SDLK_UP:           return Key::ArrowUp;
+    case SDLK_DOWN:         return Key::ArrowDown;
+    case SDLK_RIGHT:        return Key::ArrowRight;
+    case SDLK_LEFT:         return Key::ArrowLeft;
+    case SDLK_INSERT:       return Key::Insert;
+    case SDLK_HOME:         return Key::Home;
+    case SDLK_END:          return Key::End;
+    case SDLK_PAGEUP:       return Key::PageUp;
+    case SDLK_PAGEDOWN:     return Key::PageDown;
+    case SDLK_F1:           return Key::F1;
+    case SDLK_F2:           return Key::F2;
+    case SDLK_F3:           return Key::F3;
+    case SDLK_F4:           return Key::F4;
+    case SDLK_F5:           return Key::F5;
+    case SDLK_F6:           return Key::F6;
+    case SDLK_F7:           return Key::F7;
+    case SDLK_F8:           return Key::F8;
+    case SDLK_F9:           return Key::F9;
+    case SDLK_F10:          return Key::F10;
+    case SDLK_F11:          return Key::F11;
+    case SDLK_F12:          return Key::F12;
+    case SDLK_F13:          return Key::F13;
+    case SDLK_F14:          return Key::F14;
+    case SDLK_F15:          return Key::F15;
+    case SDLK_NUMLOCK:      return Key::NumLock;
+    case SDLK_SCROLLOCK:    return Key::ScrollLock;
+    case SDLK_RSHIFT:       return Key::RightShift;
+    case SDLK_LSHIFT:       return Key::LeftShift;
+    case SDLK_RCTRL:        return Key::RightControl;
+    case SDLK_LCTRL:        return Key::LeftControl;
+    case SDLK_RALT:         return Key::RightAlt;
+    case SDLK_LALT:         return Key::LeftAlt;
+    case SDLK_LSUPER:       return Key::LeftWindows;
+    case SDLK_RSUPER:       return Key::RightWindows;
+    case SDLK_SYSREQ:       return Key::SysRq;
+    case SDLK_MENU:         return Key::AppMenu;
+    case SDLK_POWER:        return Key::Power;
+    default:                return 0;
+    }
+    return 0;
 }
--- src/GuiMgr.h
+++ src/GuiMgr.h
@@ -4,6 +4,7 @@
 
 #include <SDL.h>
 #include "Singleton.h"
+#include <CEGUI.h>
 
 class GuiMgr : public Singleton<GuiMgr>
 {
@@ -19,6 +20,26 @@ class GuiMgr : public Singleton<GuiMgr>
     void handle_mouse_up(Uint8 button);
     void handle_mouse_down(Uint8 button);
 
+    // Hide or show the console
+    void setConsoleVisible(bool visible);
+    // return true if console is visible, false if is hidden
+    bool isConsoleVisible();
+    // Register our handler functions
+    void registerHandlers();
+    // Handle when we press Enter after typing
+    bool handle_TextSubmitted(const CEGUI::EventArgs& e);
+    // Handle when we press the Send button
+    bool handle_SendButtonPressed(const CEGUI::EventArgs& e);
+    // Parse the text the user submitted.
+    void parseText(CEGUI::String inMsg);
+    // Post the message to the ChatHistory listbox.
+    // with a white color default
+    void outputText(CEGUI::String inMsg,
+                    CEGUI::colour colour = CEGUI::colour(0xFFFFFFFF));
+
+
+    CEGUI::Window *m_consoleWindow;
+    bool m_consoleVisible;
 };
 
 #endif // GUIMGR_H
