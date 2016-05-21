1fdd4af2c2f081dd3d9cb2b9e4e43fd68ac396b2
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-05-21 18:02:20 +0200

    Minor change

--- src/graphics/Animation.h
+++ src/graphics/Animation.h
@@ -11,8 +11,8 @@ namespace gfx {
     class Animation
     {
     public:
-    Animation()
-        : m_currentTime(0.0f)
+        Animation()
+            : m_currentTime(0.0f)
             , m_fullTime(0.0f)
             , m_reverse(false)
             , m_playingBack(false)
