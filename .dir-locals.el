5b872e3d1f146d87dfc94db5052a4af181993160
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-11-27 17:20:03 +0100

    Added var for cmake-ide

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((nil . ((indent-tabs-mode . nil)
          (c-basic-offset . 4)
-         (compile-command . "cd ~/code/github/marian-debug && make && ./marian_d"))))
+         (compile-command . "cd ~/code/github/marian-debug && make && ./marian_d")
+         (cmake-ide-build-dir . "~/code/github/marian-debug"))))
